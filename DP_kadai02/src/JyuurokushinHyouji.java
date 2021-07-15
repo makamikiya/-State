9955e2672f2680454e19824dd39519102350af49
makamikiya <58763242+makamikiya@users.noreply.github.com>
2021-07-15 14:27:48 +0900

    Add files via upload

---
+++ DP_kadai02/src/JyuurokushinHyouji.java
@@ -0,0 +1,10 @@
+
+class JyuurokushinHyouji implements Observer{
+	public void update(Subject s) {
+		print(((Suuchi)s).getValue());
+	}
+
+	private void print(int n) {
+		System.out.println(n+"を16進数で表示します");
+	}
+}
---
+++ DP_kadai02/src/Subject.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+abstract class Subject {
+	public ArrayList<Observer> observers;
+
+	Subject(){
+		observers = newArrayList<Observer>();
+	}
+	public void attach(Observer o) {
+		observers.add(o);
+	}
+	public void detach(Observer o) {
+		observers.remove(o);
+	}
+	public void tsuuchi() {
+		for(Observer observer: observers) {
+			observer.update(this);
+		}
+	}
+}
